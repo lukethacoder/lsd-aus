23f21f934dff7486a56c6abddb720e3f4cec2cb7
Luke Secomb <luke.secomb@live.com>
2020-02-01 12:53:27 +1100

    update eslint / update build config

--- packages/tslint-config/react.js
+++ packages/tslint-config/react.js
@@ -1,32 +1,41 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   extends: [
-    'tslint:recommended',
-    'tslint-react',
-    'tslint-origin-ordered-imports-rule',
+    "tslint:recommended",
+    "tslint-react",
+    "tslint-origin-ordered-imports-rule"
   ],
   rulesDirectory: [
-    path.join(path.dirname(require.resolve('tslint-origin-ordered-imports-rule'))),
+    path.join(
+      path.dirname(require.resolve("tslint-origin-ordered-imports-rule"))
+    )
   ],
   rules: {
-    quotemark: [true, 'single', 'jsx-double'],
-    'member-access': false,
-    'member-ordering': false,
-    'object-literal-sort-keys': false,
-    'ordered-imports': false,
-    'origin-ordered-imports': [true, 'one-blank-line'],
-    'variable-name': [true, 'ban-keywords', 'check-format', 'allow-leading-underscore', 'allow-pascal-case'],
-    'max-line-length': [true, { limit: 140, 'ignore-pattern': '^import |^export {(.*?)}' }],
-    'no-default-export': true,
-    'jsx-boolean-value': false,
-    'jsx-no-lambda': false,
-    'jsx-no-multiline-js': false,
-    'interface-name': true
+    quotemark: [true, "single", "jsx-double"],
+    "member-access": false,
+    "member-ordering": false,
+    "object-literal-sort-keys": false,
+    "ordered-imports": false,
+    "origin-ordered-imports": [true, "one-blank-line"],
+    "variable-name": [
+      true,
+      "ban-keywords",
+      "check-format",
+      "allow-leading-underscore",
+      "allow-pascal-case"
+    ],
+    "max-line-length": [
+      true,
+      { limit: 140, "ignore-pattern": "^import |^export {(.*?)}" }
+    ],
+    "no-default-export": true,
+    "jsx-boolean-value": false,
+    "jsx-no-lambda": false,
+    "jsx-no-multiline-js": false,
+    "interface-name": true
   },
   linterOptions: {
-    exclude: [
-      'node_modules',
-    ],
-  },
+    exclude: ["node_modules"]
+  }
 };
